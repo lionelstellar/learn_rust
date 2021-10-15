c168b8f0ded9331911a851094c4d895441b8e031
lionelstellar <1023612737@qq.com>
2021-10-15 09:33:55 +0800

    update

--- Notes.md
+++ Notes.md
@@ -2,3 +2,4 @@
 
 [变量](variables/variables.md ':include :type=code')
 
+[数据类型](data_types/data_types.md ':include :type=code')
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,15 +1,22 @@
 fn main() {
+
+    // 不可变变量x
     let x = 5;
     println!("The value of x is: {}", x);
     // x = 6;
     // println!("The value of x is: {}", x);
 
+    // shadow
+    let x = 6;
+    println!("The value of x is: {}", x);
 
+    // 可变变量y
     let mut y = 0;
     println!("The value of y is: {}", y);
     y = 2147483647;
     println!("The value of y is: {}", y);
 
+    // 常量Z
     const Z:i64 = 100_000;
     println!("The value of z is: {}", Z);
 }
